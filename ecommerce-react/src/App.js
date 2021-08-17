b8a5d6aef1fbfb20c9168be59612b284b41a0253
kushkc12 <45266621+kushkc12@users.noreply.github.com>
2021-08-17 19:56:08 +0545

    amazon clone

--- ecommerce-react/src/App.js
+++ ecommerce-react/src/App.js
@@ -1,10 +1,104 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom"
+import Header from './components/Header';
+
 
 function App() {
   return (
-    
-  );
+<Router>
+  <div class="app">
+ 
+ <Switch>
+<Route path="/checkout">
+  <h1>checkout</h1>
+</Route>
+
+<Route path="/">
+  <Header/>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+  <h1>Home</h1>
+
+</Route>
+
+<Route path="/login">
+  <h1>Login page</h1>
+</Route>
+ </Switch>
+
+
+
+  </div>
+</Router>
+
+    );
 }
 
 export default App;
--- ecommerce-react/src/index.js
+++ ecommerce-react/src/index.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Counter from "./components/counter"
-import Header from "./components/header"
-import Product from "./components/product"
-import Carousal from "./components/Carousel"
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header/>
-<Carousal/>
-<Product/>
-<Product/>
-<Product/>
-<Product/>
-<Product/>
-<Product/>
-<Product/>
-
-
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
